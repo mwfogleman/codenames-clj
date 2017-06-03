d4957fa7f17aa41bc4f3ca2dd7f277cbdc009bb0
Michael Fogleman <michaelwfogleman@gmail.com>
2017-06-03 11:52:26 -0400

    Add revealed? and hidden?.

--- src/codenames/moves.clj
+++ src/codenames/moves.clj
@@ -7,15 +7,28 @@
   [collection element]
   (some #(= element %) collection))
 
+(defn word-filterer [w {:keys [word]}]
+  (= word w))
+
 (defn valid-word? [game word]
   (let [words (select [ATOM :words ALL :word] game)]
     (in? words word)))
 
-(defn word-filterer [w {:keys [word]}]
-  (= word w))
+(defn revealed? [game word]
+  {:pre [(valid-word? game word)]}
+  (select [ATOM :words (filterer #(word-filterer word %)) ALL :revealed? FIRST] game))
+
+(def hidden? (complement revealed?))
 
 (defn reveal! [game word]
-  {:pre [(valid-word? game word)]}
+  {:pre [(valid-word? game word)
+         (hidden? game word)]}
+  ;; check that the word hasn't already been revealed
   (setval [ATOM :words (filterer #(word-filterer word %)) ALL :revealed?]
           true g))
 
+;; Functions to Implement:
+;; next-turn!
+;; check-winning-condition
+;; change-teams
+;; current-team
