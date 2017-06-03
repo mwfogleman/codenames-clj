e994cd45b8b00a4491d91f556bff2194e9d6725a
Michael Fogleman <michaelwfogleman@gmail.com>
2017-06-03 11:30:09 -0400

    Implement reveal!.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.13.0"]]
+                 [clj-time "0.13.0"]
+                 [com.rpl/specter "1.0.1"]]
   :main ^:skip-aot codenames.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/codenames/moves.clj
+++ src/codenames/moves.clj
@@ -1,5 +1,21 @@
 (ns codenames.moves
-  (:require [codenames.game :as game]))
+  (:require [codenames.game :as game]
+            [com.rpl.specter :refer :all]))
 
-(defn reveal [game word])
+(defn in?
+  "True if the collection contains the element."
+  [collection element]
+  (some #(= element %) collection))
+
+(defn valid-word? [game word]
+  (let [words (select [ATOM :words ALL :word] game)]
+    (in? words word)))
+
+(defn word-filterer [w {:keys [word]}]
+  (= word w))
+
+(defn reveal! [game word]
+  {:pre [(valid-word? game word)]}
+  (setval [ATOM :words (filterer #(word-filterer word %)) ALL :revealed?]
+          true g))
 
