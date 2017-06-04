84eb81d0e9a564f2832226cc2a0e0aa53bc8a085
Michael Fogleman <michaelwfogleman@gmail.com>
2017-06-04 17:04:36 -0400

    move! ?!?!?

--- src/codenames/moves.clj
+++ src/codenames/moves.clj
@@ -58,14 +58,6 @@
          (map get-attributes)
          (frequencies))))
 
-(defn- assassinate!
-  "If the word is the assassin, the current team loses. If the word is not the assassin, reveal it."
-  [game word]
-  (let [id (select-any [ATOM :words (filterer #(word-filterer "RULER" %)) ALL :identity] g)]
-    (if (= id :assassin)
-      (lose! game)
-      (reveal! game word))))
-
 (defn winner?
   "If a GAME has a winner, return true. If not, return false."
   [game]
@@ -73,28 +65,34 @@
        (select-any [ATOM :winning-team])
        (some?)))
 
-;; A player tries to make a move
-;; If it's not a valid word, they can't make the move
-;; If there's already a winner, they can't make the move
-;; If they pick the assassin, they lose
-;; Otherwise, reveal the card.
-;; Register whether they picked someone on their team, or on the other team.
-;; Check if there are remaining hidden cards for either team.
-;; If not, set a winner. (depends who has no cards remaining - they may have revealed a card for the opposite team)
-;; If they picked someone on their team, they can keep moving
-;; If they picked someone from the other team, switch to make it the other team's turn.
+(defn- get-current-team
+  [game]
+  (select-any [ATOM :current-team] game))
 
-(defn move! [game]
+(defn- get-id-of-word [game word]
+  (select-any [ATOM :words (filterer #(word-filterer word %)) ALL :identity] game))
+
+(defn move! [game word]
   {:pre [(valid-word? game word)
-         (hidden? game word)]}
-  ;; (let [winner? (fn [game] (true? (select-any [ATOM :winning-team nil?] game)))
-  ;;       frqs (get-freqs game)
-  ;;       assassin? (= 1 (get frqs [:assassin false]))
-  ;;       blue-remaining (get frqs [:blue false])
-  ;;       red-remaining (get frqs [:red false])]
-  ;;   [{:frqs frqs
-  ;;     :winner? (winner? game)
-  ;;     :blue-remaining blue-remaining
-  ;;     :red-remaining red-remaining}]
-  ;;   )
-  )
+         (hidden? game word)
+         (= false (winner? game))]}
+  (reveal! game word)
+  (let [current-team (get-current-team game)
+        id (get-id-of-word game word)
+        match-result (= id current-team) ;; Register whether they picked someone on their team, or on the other team.
+        frqs (get-freqs game)
+        blue-remaining (get frqs [:blue false])
+        red-remaining (get frqs [:red false])]
+    (cond (= id :assassin) (lose! game)
+          (and (> blue-remaining 0) (> red-remaining 0)) ;; Check if there are remaining hidden cards for either team.
+          ;; If they picked someone on their team, they can keep moving
+          ;; If they picked someone from the other team, switch to make it the other team's turn.
+          (if (true? match-result)
+            game
+            (next-turn! game))
+          :else
+          (if (true? match-result)
+            ;; If the card picked was theirs, win!
+            (win! game)
+            ;; Otherwise, lose!
+            (lose! game)))))
